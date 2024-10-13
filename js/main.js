b1b84b4669d3368512598f4672f122bbe3e67e0c
ZayYanTun1999 <zayyan0989@gmail.com>
2024-10-13 16:11:12 +0630

    project setup

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const toggle = document.querySelector("#toggle")
+const menu = document.querySelector("#menu")
+
+toggle.onclick = () => {
+    toggle.querySelector("i").classList.toggle("fa-xmark")
+    menu.classList.toggle('hidden')
+}
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        'items': 4,
+        'margin': 30,
+        'autoplay': true,
+        'autoplayTimeout': 3000,
+        'autoplayHoverPause': true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            900: {
+                items: 3
+            },
+            1100: {
+                items: 4
+            }
+        }
+    });
+});
+
+const up = document.querySelector("#up");
+up.onclick = () => {
+    window.scrollTo(0,0)
+}
+
+window.onscroll = () => {
+    if(window.scrollY >100) {
+        up.classList.remove('hidden')
+    } else {
+        up.classList.add('hidden')
+    }   
+}
